6a6bfc74fbb72690cedeaf50040bcc340f422da4
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-08 11:41:58 +0300

    description added

--- 1. Two Sum/Solution.js
+++ 1. Two Sum/Solution.js
@@ -4,15 +4,21 @@
  * @return {number[]}
  */
 
-
- var twoSum = function (nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                if (i !== j) {
-                    return [i, j];
-                }
-            }
+var twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] == target) {
+        if (i !== j) {
+          return [i, j];
         }
+      }
     }
-}
+  }
+};
+
+/* ----------------------------------------------------Question--------------------------------------------------------------*/
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
