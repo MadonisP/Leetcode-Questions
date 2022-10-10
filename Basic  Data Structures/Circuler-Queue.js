1d841f01c7882b491dcdf23d63876dc5373a881f
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-10 14:15:31 +0300

    Circular Queue data structure added

---
+++ Basic  Data Structures/Circuler-Queue.js
@@ -0,0 +1,44 @@
+class circulatQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.rear = -1;
+    this.front = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return this.currentLength === this.capacity;
+  }
+
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        // first element enqueue
+        this.front = this.rear;
+      }
+    }
+  }
+
+  dequeue() {
+    if (this.isEmpty) {
+      return null;
+    }
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front + 1) % this.capacity;
+    this.currentLength -= 1;
+    if (this.isEmpty()) {
+      this.front = -1;
+      this.rear = -1;
+    }
+    return item;
+  }
+}
