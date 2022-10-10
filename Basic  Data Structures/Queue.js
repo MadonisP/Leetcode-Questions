41dd3dd88b1e02047d5f68d872f39a2bae9175cb
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-10 13:08:41 +0300

    Faster Queue data structure added

--- Basic  Data Structures/Queue.js
+++ Basic  Data Structures/Queue.js
@@ -1,5 +1,5 @@
 /////////////////////*Yavas*////////////////////////////////////////////////////////////////////////////////////*Slow*////////////////////////////
-class Queue {
+class SlowQueue {
   constructor() {
     this.items = [];
   }
@@ -30,18 +30,66 @@ class Queue {
   }
 }
 
-const queue = new Queue()
+const queue = new SlowQueue();
 
-console.log(queue.isEmpty())
+console.log(queue.isEmpty());
 
-queue.enqueue(10)
-queue.enqueue(30)
-queue.enqueue(20)
-console.log(queue.size())
-queue.print()
+queue.enqueue(10);
+queue.enqueue(30);
+queue.enqueue(20);
+console.log(queue.size());
+queue.print();
 
-console.log(queue.dequeue())
-console.log(queue.peek())
-
+console.log(queue.dequeue());
+console.log(queue.peek());
 
 /////////////////////*Hizli*////////////////////////////////////////////////////////////////////////////////////*Fast*////////////////////////////
+/////////////////////*Example image: https://www.tutorialride.com/images/data-structures/queue-array.jpeg *////////////////////////////
+
+class FastQueue {
+  constructor() {
+    this.items = {};
+    thir.rear = 0;
+    thir.front = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+const fastQueue = new FastQueue();
+console.log(fastQueue.isEmpty());
+
+fastQueue.enqueue(10);
+fastQueue.enqueue(20);
+fastQueue.enqueue(30);
+console.log(FastQueue.size());
+fastQueue.print();
+
+console.log(fastQueue.dequeue());
+console.log(fastQueue.peek());
