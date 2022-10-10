0a053368825d40afb08f1b6134b26015bc9c845f
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-10 17:08:55 +0300

    Linked List data structure updated

--- Basic  Data Structures/linked-List.js
+++ Basic  Data Structures/linked-List.js
@@ -62,6 +62,107 @@ class LinkedList {
     }
   }
 
+  /* removeFromFront() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size--;
+    return value;
+  } */
+
+  /*   removeFromEnd() {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const value = this.tail.value;
+    if (this.size === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      let prev = this.head;
+      while (prev.next !== this.tail) {
+        prev = prev.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+    }
+    this.size--;
+    return value;
+  } */
+
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+    let removedNode;
+    if (index === 0) {
+      removedNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+      removedNode = prev.next;
+      prev.next = removedNode.next;
+    }
+    this.size--;
+    return removedNode.value;
+  }
+
+  removeValue(value) {
+    if (this.isEmpty()) {
+      return null;
+    }
+    if (this.head.value === value) {
+      this.head = this.head.next;
+      this.size--;
+      return value;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next.value !== value) {
+        prev = prev.next;
+      }
+      if (prev.next) {
+        removedNode = prev.next;
+        prev.next = removedNode.next;
+        this.size--;
+        return value;
+      }
+      return null;
+    }
+  }
+
+  search(value) {
+    if (this.isEmpty()) {
+      return -1;
+    }
+    let i = 0;
+    let curr = this.head;
+    while (curr) {
+      if (curr.value === value) {
+        return i;
+      }
+      curr = curr.next;
+      i++;
+    }
+    return -1;
+  }
+
+  reverse() {
+    let prev = null;
+    let curr = this.head;
+    while (curr) {
+      let next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    this.head = prev;
+  }
+
   print() {
     if (this.isEmpty()) {
       console.log("List is empty");
@@ -82,11 +183,38 @@ console.log("List is empty", list.isEmpty());
 console.log("List size", list.getSize());
 
 //list.prepend(10);
-list.append(10);
+////list.append(10);
+list.insert(10, 0);
 list.print();
 
-list.prepend(20);
+////list.prepend(20);
 //list.append(20)
-list.prepend(30);
+////list.prepend(30);
 //list.append(30)
+list.insert(20, 0);
 list.print();
+
+list.insert(30, 1);
+list.print();
+
+list.insert(40, 2);
+list.print();
+console.log(list.search(40));
+list.removeFrom(1);
+console.log(list.getSize());
+
+/////////////////////////////////////////////////////////////////////////
+
+/* 
+const list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.prepend(0);
+list.print();
+console.log(list.getSize());
+list.removeFromFront();
+list.print();
+list.removeFromEnd();
+list.print();
+ */
