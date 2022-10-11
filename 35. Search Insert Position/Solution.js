5e98f3fe9977ea46c3e4233af7efa66aebc253bf
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-11 11:35:18 +0300

    description of 35 updated

--- 35. Search Insert Position/Solution.js
+++ 35. Search Insert Position/Solution.js
@@ -20,6 +20,10 @@ var searchInsert = function (nums, target) {
   }
 };
 
+
+// by the way this solution is beats 97% !!!!!!
+
+
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
 
 /* Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
