9705ca5d313b8046b7f6a6079fe957fb62666457
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-12 22:20:25 +0300

    Solution of 977 added

--- 977. Squares of a Sorted Array/Solution.js
+++ 977. Squares of a Sorted Array/Solution.js
@@ -1,4 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var sortedSquares = function (nums) {
+    const sorted = (a, b) => {
+        if (a < b) return -1;
+        if (b < a) return 1;
+        return 0;
+    }
 
+    arr = []
+    for (let i = 0; i < nums.length; i++) {
+        square = Math.pow(nums[i], 2)
+        arr.push(square)
+    }
+    arr.sort(sorted)
+    return arr
+};
 
 
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
