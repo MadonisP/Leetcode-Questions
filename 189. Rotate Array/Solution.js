ab92d4356fe6d8956351076967d67e50cc38a7bd
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-12 23:22:14 +0300

    Solution of 189 added

--- 189. Rotate Array/Solution.js
+++ 189. Rotate Array/Solution.js
@@ -1,3 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function (nums, k) {
+  for (let i = nums.length - 1; i >= 0; i--) {// Burada sayilari atiyoruz ancak elimizde 7 uzunlugunda degil 10 uzunlugunda bir array olusuyor
+    nums[i + k] = nums[i]; //length -1 dememizin sebebi length 1 den basliyor ancak array 0 dan bunlari esliyoruz
+  }
+
+  for (let j = k - 1; j >= 0; j--) {//bastaki 3 gereksiz elemani atarak arrayi duzenliyoruz
+    nums[j] = nums.pop();
+  }
+};
+
 
 
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
