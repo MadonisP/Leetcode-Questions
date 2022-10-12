0282a0a24aebbefe907e0ad79b9b19294100ba13
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-12 22:14:12 +0300

    Question of 977 added

---
+++ 977. Squares of a Sorted Array/Solution.js
@@ -0,0 +1,15 @@
+
+
+
+/* ----------------------------------------------------Question--------------------------------------------------------------*/
+
+/* 
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+*/
+
+/* ----------------------------------------------------Example--------------------------------------------------------------*/
+
+/* 
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+*/
