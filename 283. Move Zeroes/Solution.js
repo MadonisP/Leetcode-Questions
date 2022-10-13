9eef5c0757cae448883088ea2bac8fb24546cd7e
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-13 18:11:13 +0300

    Solutionn of 283 added

--- 283. Move Zeroes/Solution.js
+++ 283. Move Zeroes/Solution.js
@@ -1,3 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+ var ZC = 0
+ var moveZeroes = function (nums) {
+     let Pointer = 0;
+     for (let i = 0; i < nums.length; i++) {
+         const val = nums[i];
+         nums[i] = 0;
+         if (val !== 0) {
+             nums[Pointer] = val;
+             Pointer++;
+         }
+     }
+ };
+
+
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
 
 /* 
