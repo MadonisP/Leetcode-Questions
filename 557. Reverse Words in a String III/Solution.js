978c46789ffc2c26d48017ddc82056a75470661e
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-14 10:27:01 +0300

    Solution of 557 added

--- 557. Reverse Words in a String III/Solution.js
+++ 557. Reverse Words in a String III/Solution.js
@@ -1,3 +1,23 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+ var reverseWords = function(s) {
+    var myArr=s.split(' ')
+    var test=myArr[0].split("")
+    let p1=0
+    let p2=test.length-1
+    while(p1,p2){
+        let holder=test[p1]
+        test[p1]=test[p2]
+        test[p2]=holder
+
+        p1++
+        p2--
+    }
+    return test.join('')
+};
+
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
 
 /* 
