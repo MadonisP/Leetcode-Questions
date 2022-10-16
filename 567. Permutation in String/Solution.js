088c0f4a83d38249573f0dbd782365d0c50aabf9
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-16 23:23:09 +0300

    Solution of 567 added

--- 567. Permutation in String/Solution.js
+++ 567. Permutation in String/Solution.js
@@ -1,3 +1,51 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var checkInclusion = function (s1, s2) {
+  let windowStart = 0;
+  let matched = 0;
+  let charFreq = {};
+
+  for (let i = 0; i < s1.length; i++) {
+    let char = s1[i];
+    if (charFreq[char] === undefined) {
+      charFreq[char] = 1;
+    } else {
+      charFreq[char]++;
+    }
+  }
+
+  for (let windowEnd = 0; windowEnd < s2.length; windowEnd++) {
+    let rightChar = s2[windowEnd];
+
+    if (rightChar in charFreq) {
+      charFreq[rightChar] -= 1;
+      if (charFreq[rightChar] === 0) {
+        matched += 1;
+      }
+    }
+
+    if (matched === Object.keys(charFreq).length) {
+      return true;
+    }
+
+    if (windowEnd >= s1.length - 1) {
+      let leftChar = s2[windowStart];
+      windowStart += 1;
+      if (leftChar in charFreq) {
+        if (charFreq[leftChar] === 0) {
+          matched -= 1;
+        }
+        charFreq[leftChar] += 1;
+      }
+    }
+  }
+
+  return false;
+};
+
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
 
 /* 
