2125ba04a438c3f7bd378b73393bcae8a39bfacf
Mert süha igaç <95320998+MadonisP@users.noreply.github.com>
2022-10-21 01:40:05 +0300

    Question of 542 added

--- 116. Populating Next Right Pointers in Each Node/Solution.js
+++ 116. Populating Next Right Pointers in Each Node/Solution.js
@@ -12,21 +12,19 @@
  * @param {Node} root
  * @return {Node}
  */
- var connect = function (root) {
-    let ptr = root;
-    while (root && root.left) {
-        let temp = root;
-        while (temp) {
-            temp.left.next = temp.right;
-            temp.right.next = temp.next ? temp.next.left : null;
-            temp = temp.next;
-        }
-        root = root.left;
+var connect = function (root) {
+  let ptr = root;
+  while (root && root.left) {
+    let temp = root;
+    while (temp) {
+      temp.left.next = temp.right;
+      temp.right.next = temp.next ? temp.next.left : null;
+      temp = temp.next;
     }
-    return ptr;
+    root = root.left;
+  }
+  return ptr;
 };
-
-
 
 /* ----------------------------------------------------Question--------------------------------------------------------------*/
 
